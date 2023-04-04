d359cc0d24abb1223ced18b017faf1b493d31bb3
stone <sitte@163.com>
2023-04-04 14:25:25 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+# from gevent import monkey
+# from geventwebsocket.handler import WebSocketHandler
+# from geventwebsocket.logging import create_logger
+# from gevent.pywsgi import WSGIServer
+from flask_socketio import SocketIO, send, emit
+
+
+# monkey.patch_all()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+# socketio = SocketIO(app, logger=True, engineio_logger=True)
+# socketio = SocketIO(app, async_mode='gevent', transport='websocket')
+# socketio = SocketIO(app, async_mode='gevent', logger=True, engineio_logger=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print('Client connected')
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message: ' + message)
+    send(message, broadcast=True)
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('Received event: ' + str(json))
+    emit('my response', json, broadcast=True)
+
+@socketio.on_error_default
+def default_error_handler(e):
+    print('An error occurred:', e)
+
+if __name__ == '__main__':
+    # http_server = WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
+    # http_server.serve_forever()
+    # socketio.run(app, server=http_server)
+    socketio.run(app)
